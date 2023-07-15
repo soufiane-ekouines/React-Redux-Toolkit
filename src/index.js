194ec5f8f705959bab79284b3d89af6ce22a4c1c
soufiane-ekouines <soufianeekouines@gmail.com>
2023-07-15 19:59:04 +0100

    erreur last commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './app/store';
+import {store} from './app/store';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
